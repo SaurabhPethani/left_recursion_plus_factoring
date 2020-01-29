f218b3874c3f701dda190fdbb3560cb00a147300
SaurabhPethani <spethani28@gmail.com>
2020-01-29 17:02:42 +0530

    Left Factoring V2.0

--- left_fact.py
+++ left_fact.py
@@ -1,43 +1,113 @@
-production = input("Enter the Production: ")
-prod_list = production.split('|')
+productions = input("Enter the Production: ")
+prod_list = productions.split('|')
 copy_prod_list = [v for v in prod_list]
+main_prod = {}
+no_match = []
 
-# def search_for_longest(lst):
-#     start = lst[0][0]
-#     flag = 0
-#     if len(lst) == 1:
-#         return start
-#     for index in range(1, len(lst)):
-#         if  lst[index][0] != start:
-#             flag = 1
-def getProductions():
-    global prod_list, copy_prod_list
-    startChar = copy_prod_list[0][0]
-    similarProd = []
-    for prod in prod_list:
-        if prod[0] == startChar:
-            similarProd.append(prod)
+def getProductions(production):
+    global main_prod, copy_prod_list, no_match
+    production = sorted(production)
+    print(production)
+    if len(production)<= 1:
+        return
+    start = production[0][0]
+    prod_dict = {}
+
+    similar = []
+    print(similar)
+    print(start)
+    print("Prod before for -> ",production)
+    for prod in production:
+        print("Prod - > ",prod)
+        print("prod[0] == start -> ",prod[0] == start)
+        if prod[0] == start:
+            similar.append(prod[1:])
+            print("Before remove ",production)
             copy_prod_list.remove(prod)
+            print("After remove ",production)
+        else:
+            no_match.append(prod + " | ")
     
-    similarProd[0:0] = startChar
+    if len(similar) >= 2:
+        prod_dict[start] = similar
+        main_prod.update(prod_dict)
+    print(similar)
+    print("MainProd = ", main_prod)
+    
+    print(copy_prod_list)
+    getProductions(copy_prod_list)
 
-    return similarProd
-length = len(copy_prod_list)
-split_prod = []
-while length:
-    split_prod.append(getProductions())
-    length = len(copy_prod_list)
+def get2ndProd(prod, start):
+    global main_prod
+    prod = sorted(prod, reverse=True)
+    print("Production -> ",prod)
+    new_prod=[]
+    mini_prod = {}
+    similar=[]
+    never_enter = []
+    startChar = prod[0][0]
 
-count= 1
-root_prod = []
-new_prod = {}
-for product in split_prod:
-    symbol = 'A{}'.format(count)
+    for p in prod:
+        if p[0] == startChar:
+            similar.append(p[1:])
+        else:
+            never_enter.append(p)
+    if len(similar) == 1:
+        new_prod = prod
+        print(new_prod)
+        print(mini_prod)
+        
+    elif len(similar) >= 2:
+        mini_prod[startChar] = similar
+
+        new_prod.append(mini_prod)
+        if len(never_enter) >= 1:
+            new_prod.extend(never_enter)
+        print("never enter prod -> ", never_enter)
+        print("Elif new Prod", new_prod)
+        print("Elif mini prod", mini_prod)
+
+    main_prod[start] = new_prod
+
+getProductions(prod_list)
+for key, value in main_prod.items():
+    get2ndProd(value, key)
+print(main_prod)
+
+
+final_production = []
+count=1
+production_list = []
+for key,values in main_prod.items():
+    final_production.append(key+'A{}'.format(count)+"|")
+    flag = 0
+    for prod in values:
+        if type(prod) is not str:
+            flag = 1
+    if flag == 0:
+        # count += 1
+        A1 = "A{}".format(count)+"-> "+"| ".join(values)
+        production_list.append(A1)
+    
+    elif flag == 1:
+        # count += 1
+        A2 = 'A{}'.format(count)+" -> "
+        for prod in values:
+            if type(prod) is str:
+                A2 += " | "+prod + " | "
+            if type(prod) is dict:
+                A3=''
+                for key, value in prod.items():
+                    count += 1
+                    A2 += key +"A{}".format(count)
+                    A3 += "A{} -> ".format(count) + "| ".join(value)
+                production_list.append(A3)    
+        production_list.append(A2)
     count += 1
-    start = product.pop(0)
-    new_prod[symbol]=[v[1:] for v in product]
-    factored_prod = start+symbol
-    root_prod.append(factored_prod)
-
-print("New Production: ", new_prod)
-print("Main production: ", root_prod)
+final_production.extend(no_match)
+print("A -> ", end='')
+for item in final_production:
+    print(item,end='')
+print()
+for item in production_list:
+    print(item)
